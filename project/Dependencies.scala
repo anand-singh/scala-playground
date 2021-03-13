2b759ccdf663505ab8bfac0fe43d67101c99fd44
Scala Steward <me@scala-steward.org>
2021-03-13 12:08:44 +0100

    Update kind-projector from 0.11.0 to 0.11.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
         "org.typelevel" %% "discipline-scalatest" % "2.0.0"
 
       val `kind-projector` =
-        "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
+        "org.typelevel" %% "kind-projector" % "0.11.3" cross CrossVersion.full
 
       val catsCore = "org.typelevel" %% "cats-core" % "2.4.2"
     }
